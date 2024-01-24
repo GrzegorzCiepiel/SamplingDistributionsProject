12888bd81735645bc3ff62d94cc711645d3cf83c
Grzegorz Ciepiel <grzegorz@ciepiel.pl>
2024-01-24 12:14:24 +0100

    add main functionalities

--- helper_functions.py
+++ helper_functions.py
@@ -27,7 +27,7 @@ def choose_statistic(x, sample_stat_text):
 
 def population_distribution(population_data):
   # plot the population distribution
-  sns.histplot(population_data, stat='density', palette='flare')
+  sns.histplot(population_data, stat='density', color=("#5A9"))
   plt.title(f"Population Distribution")
   # remove None label
   plt.xlabel('')
@@ -37,24 +37,24 @@ def population_distribution(population_data):
 
 def sampling_distribution(population_data, samp_size, stat):
 # list that will hold all the sample statistics
-sample_stats = []
-for i in range(500):
-  # get a random sample from the population of size samp_size
-  samp = np.random.choice(population_data, samp_size, replace=False)
-  # calculate the chosen statistic (mean, minimum, or variance) of the sample
-  sample_stat = choose_statistic(samp, stat)
-  # add sample_stat to the sample_stats list
-  sample_stats.append(sample_stat)
+  sample_stats = []
+  for i in range(500):
+    # get a random sample from the population of size samp_size
+    samp = np.random.choice(population_data, samp_size, replace=False)
+    # calculate the chosen statistic (mean, minimum, or variance) of the sample
+    sample_stat = choose_statistic(samp, stat)
+    # add sample_stat to the sample_stats list
+    sample_stats.append(sample_stat)
 
-pop_statistic = round(choose_statistic(population_data, stat), 2)
+  pop_statistic = round(choose_statistic( population_data, stat), 2)
 
-# plot the sampling distribution
-sns.histplot(sample_stats, stat='density', bins=30, color='purple')
-plt.title(f"Sampling Distribution of the {stat} \nMean of the Sample {stat}s: {round(np.mean(sample_stats), 2)} \n "
-          f"Population {stat}: {pop_statistic}")
-plt.axvline(pop_statistic, color='green', linestyle='dashed', label=f'Population {stat}')
-# plot the mean of the chosen sample statistic for the sampling distribution
-plt.axvline(np.mean(sample_stats), color='orange', linestyle='dashed', label=f'Mean of the Sample {stat}s')
-plt.legend()
-plt.show()
-plt.clf()
+  # plot the sampling distribution
+  sns.histplot(sample_stats, stat='density', bins=30, color='purple')
+  plt.title(f"Sampling Distribution of the {stat} \nMean of the Sample {stat}s: {round(np.mean(sample_stats), 2)} \n "
+            f"Population {stat}: {pop_statistic}")
+  plt.axvline(pop_statistic, color='green', linestyle='dashed', label=f'Population {stat}')
+  # plot the mean of the chosen sample statistic for the sampling distribution
+  plt.axvline(np.mean(sample_stats), color='orange', linestyle='dashed', label=f'Mean of the Sample {stat}s')
+  plt.legend()
+  plt.show()
+  plt.clf()
---
+++ main.py
@@ -0,0 +1,33 @@
+from helper_functions import choose_statistic, population_distribution, sampling_distribution
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns
+
+# Read data from csv file
+spotify_data = pd.read_csv('spotify_data.csv')
+# Preview the dataset
+print(spotify_data.head())
+# Prepare dataset to work on
+song_tempos = spotify_data.tempo
+
+
+# Plot the population distribution with the mean labeled
+population_distribution(song_tempos)
+# Sampling distribution of the sample mean
+sampling_distribution(song_tempos, 30, "Mean" )
+# Sampling distribution of the sample minimum
+sampling_distribution(song_tempos, 30, "Minimum")
+# Sampling distribution of the sample variance
+
+# Calculate the population mean and standard deviation
+population_mean = np.mean(song_tempos)
+population_std = np.std(song_tempos)
+# Calculate the standard error
+standard_error = population_std/30**0.5
+print(standard_error)
+# Calculate the probability of observing an average tempo of 140bpm or lower from a sample of 30 songs
+print(stats.norm.cdf(140, 147.74, 3.35))
+# Calculate the probability of observing an average tempo of 150bpm or higher from a sample of 30 songs
+print(1 - stats.norm.cdf(150, 147.74, 3.35))
